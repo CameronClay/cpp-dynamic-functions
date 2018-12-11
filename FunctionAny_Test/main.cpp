64876e6e41d284cf39a021c2ad77c5abd7d6a3ef
Cameron <cameronpclay@gmail.com>
2018-12-11 15:26:40 -0700

    Updated test code.

--- FunctionAny_Test/main.cpp
+++ FunctionAny_Test/main.cpp
@@ -66,6 +66,13 @@ using namespace f_traits;
 template<typename... Ts>
 using tl = t_list::type_list<Ts...>;
 
+template<typename T1>
+struct IS_TYPE
+{
+	template<typename T2>
+	static constexpr bool value = std::is_same_v<T1, T2>;
+};
+
 int main()
 {
 	std::string local = "the world";
@@ -105,37 +112,38 @@ int main()
 	// Catch and process the return value
 	auto rt_visitor = [](const auto& ret)
 	{
-		using RT = std::decay_t<decltype(ret)>;
+		using RT    = std::decay_t<decltype(ret)>;
+		using RT_IS = IS_TYPE<RT>;
 
 		// Not possible for RT not to exist in RTS_UNIQUE but nice to check anyways
 		if constexpr (FUNC_ANY::RTS_UNIQUE::contains<RT>)
 		//if constexpr(FUNC_ANY::RTS_UNIQUE::filter<std::is_arithmetic>::contains<RT>)
 		{
-			if constexpr      (std::is_same_v<RT, NO_CALL>)
+			if constexpr      (RT_IS::value<NO_CALL>)
 			{
 				//std::cout << "Func was not called" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, VOID>)
+			else if constexpr (RT_IS::value<VOID>)
 			{
 				//std::cout << "Func returned with type: void" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, int>)
+			else if constexpr (RT_IS::value<int>)
 			{
 				std::cout << "Func returned " << ret << " with type: int" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, float>)
+			else if constexpr (RT_IS::value<float>)
 			{
 				std::cout << "Func returned " << ret << " with type: float" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, std::string>)
+			else if constexpr (RT_IS::value<std::string>)
 			{
 				std::cout << "Func returned " << ret << " with type: string" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, A>)
+			else if constexpr (RT_IS::value<A>)
 			{
 				std::cout << "Func returned {" << ret.m_i << ", " << ret.m_f << "} with type: A" << std::endl;
 			}
-			else if constexpr (std::is_same_v<RT, A*>)         // Reference converted to pointer type
+			else if constexpr (RT_IS::value<A*>)         // Reference converted to pointer type
 			{
 				std::cout << "Func returned " << ret << ", *A = {" << ret->m_i << ", " << ret->m_f << "} with type: A*" << std::endl;
 			}
